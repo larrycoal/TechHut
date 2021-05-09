193fa9c7a401169f7daec54019abbba8ba0df6f9
Olayiwola Olanrewaju <larry_coal@outlook.com>
2021-05-09 14:19:38 +0100

    todo

--- src/Store/actions/todoList.js
+++ src/Store/actions/todoList.js
@@ -1,8 +1,5 @@
 import {CREATE_TODO} from './types'
-//import axios from 'axios'
 
-//const UserServer = "/api/user"
-    //let response = axios.post(`${UserServer}/login`,datatoSubmit).then((res)=>res.data)
 
 export const createTodo = (todo)=>{
     return {
